108ebef74be2a41591d4a25ab4ecacc9bae7c621
Reinier Koffijberg <reinierkof@gmail.com>
2025-08-01 10:56:37 +0200

    first iteration of completion within session buffer

--- magik-company.el
+++ magik-company.el
@@ -46,6 +46,9 @@
 (defvar magik-company--slots-candidates nil)
 (defvar magik-company--exemplar-candidate nil)
 (defvar magik-company--initialised? nil)
+(defvar magik-company--major-mode nil)
+(defvar magik-company--session-mode "magik-session-mode")
+(defvar magik-company--buffer-mode "magik-ts-mode")
 
 (defgroup magik-company nil
   "Company back-end for Magik code completion."
@@ -64,6 +67,7 @@
   "Set up buffer for `magik-company-mode`."
   (make-local-variable 'company-backends)
   (add-to-list 'company-backends 'magik-company)
+  (setq magik-company--major-mode major-mode)
   (unless magik-company--initialised?
     (advice-add #'magik-transmit-region :after #'magik-company-reload-cache)
     (advice-add #'magik-session-kill-process :after #'magik-company--exit-cb-buffers)
@@ -86,15 +90,34 @@ COMMAND, ARG, IGNORED"
     (kind (magik-company--kind arg))
     (post-completion (magik-company--post-completion arg))))
 
+(defun magik-company--prefix ()
+  "Prefix and what to load based on those the current point."
+  (if (or (magik-company--in-comment)
+	  (magik-company--in-string)
+          (and (string= magik-company--major-mode magik-company--session-mode)
+               (not (magik-company--session-within-typeable-area))))
+      (setq magik-company-prefix-at-methods nil
+	    magik-company-prefix-at-conditions nil
+	    magik-company-prefix-at-dynamics nil
+	    magik-company-prefix-at-globals nil
+	    magik-company-prefix-at-slot nil
+	    magik-company-prefix-at-objects nil)
+    (progn
+      (setq magik-company-prefix-at-methods (magik-company--at-method-prefix)
+	    magik-company-prefix-at-conditions (magik-company--at-raise-condition-prefix)
+	    magik-company-prefix-at-dynamics (magik-company--at-dynamic-prefix)
+	    magik-company-prefix-at-globals (magik-company--at-global-prefix)
+	    magik-company-prefix-at-slot (magik-company--at-slot-prefix)
+	    magik-company-prefix-at-objects (magik-company--at-object-prefix)))
+    (magik-company--determine-cur-prefix)))
+
 (defun magik-company--candidates ()
   "Generate a list of completion candidates."
   (magik-company--load-source-caches)
-  (magik-company--update-buffer-caches)
 
   (let ((magik-candidates '()))
-    (when magik-company-prefix-at-slot
-      (setq magik-company--slots-candidates (magik-company--filter-candidates magik-company--slots-cache magik-candidates))
-      (setq magik-candidates (append magik-candidates magik-company--slots-candidates)))
+    (when (string= magik-company--major-mode magik-company--buffer-mode)
+      (magik-company--buffer-local-candidates magik-candidates))
 
     (when (or magik-company-prefix-at-globals
 	      magik-company-prefix-at-dynamics)
@@ -104,16 +127,7 @@ COMMAND, ARG, IGNORED"
 
     (when magik-company-prefix-at-objects
       (setq magik-company--objects-candidates (magik-company--filter-candidates magik-company--objects-source-cache magik-candidates))
-      (setq magik-candidates (append magik-candidates magik-company--objects-candidates))
-
-      (setq magik-company--params-candidates (magik-company--filter-candidates magik-company--params-cache magik-candidates))
-      (setq magik-candidates (append magik-candidates magik-company--params-candidates))
-
-      (setq magik-company--variables-candidates (magik-company--filter-candidates magik-company--variables-cache magik-candidates))
-      (setq magik-candidates (append magik-candidates magik-company--variables-candidates))
-
-      (setq magik-company--exemplar-candidate (magik-company--filter-candidates magik-company--classname-cache magik-candidates))
-      (setq magik-candidates (append magik-candidates magik-company--exemplar-candidate)))
+      (setq magik-candidates (append magik-candidates magik-company--objects-candidates)))
 
     (when magik-company-prefix-at-methods
       (setq magik-company--class-method-candidates (magik-company--filter-candidates (magik-company--method-candidates magik-company-cur-prefix) magik-candidates))
@@ -130,6 +144,23 @@ COMMAND, ARG, IGNORED"
     ;; in case we need it we can use this one.
     ;; (setq magik-candidates (delete-dups magik-candidates))
     magik-candidates))
+
+(defun magik-company--buffer-local-candidates(magik-candidates)
+  "Add to the list of completion MAGIK-CANDIDATES with buffer-local functions."
+  (magik-company--update-buffer-caches)
+  (when magik-company-prefix-at-slot
+    (setq magik-company--slots-candidates (magik-company--filter-candidates magik-company--slots-cache magik-candidates))
+    (setq magik-candidates (append magik-candidates magik-company--slots-candidates)))
+
+  (when magik-company-prefix-at-objects
+    (setq magik-company--params-candidates (magik-company--filter-candidates magik-company--params-cache magik-candidates))
+    (setq magik-candidates (append magik-candidates magik-company--params-candidates))
+
+    (setq magik-company--variables-candidates (magik-company--filter-candidates magik-company--variables-cache magik-candidates))
+    (setq magik-candidates (append magik-candidates magik-company--variables-candidates))
+
+    (setq magik-company--exemplar-candidate (magik-company--filter-candidates magik-company--classname-cache magik-candidates))
+    (setq magik-candidates (append magik-candidates magik-company--exemplar-candidate))))
 
 (defun magik-company--filter-candidates (new-candidates existing-candidates)
   "Filter NEW-CANDIDATES.
